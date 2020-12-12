f2991f5d478d4a90dcedae73f15c2c332e6f3b33
weijun <2862786390@qq.com>
2020-12-12 17:18:55 +0800

    单例模式

--- src/main/java/DesignPatterns/singleton/Sin05.java
+++ src/main/java/DesignPatterns/singleton/Sin05.java
@@ -13,10 +13,10 @@ public class Sin05 {
     }
 
     public static Sin05 getINSTANCE() {
-        if (INSTANCE == null){
-            synchronized(Sin05.class){
-                if (INSTANCE == null){
-                    INSTANCE =  new Sin05();
+        if (INSTANCE == null) {
+            synchronized (Sin05.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new Sin05();
                 }
             }
         }
@@ -25,10 +25,10 @@ public class Sin05 {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i <100 ; i++) {
+        for (int i = 0; i < 100; i++) {
             new Thread(() -> {
-                System.out.println(Thread.currentThread().getName()+"\t"+ Sin05.getINSTANCE().hashCode());
-            }, i+"").start();
+                System.out.println(Thread.currentThread().getName() + "\t" + Sin05.getINSTANCE().hashCode());
+            }, i + "").start();
         }
 
     }
